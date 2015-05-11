0703fc3579169e5abea16829953928d88deb022b
jhymer <jhmartinez1991@gmail.com>
2015-05-11 14:51:33 -0500

    generate initial test

--- client/app/tweetservice/tweetservice.service.js
+++ client/app/tweetservice/tweetservice.service.js
@@ -1,17 +1,74 @@
 'use strict';
 
 angular.module('twittexpressApp')
-.service('tweetservice', function ($http) {
+.service('tweetservice', function ($http,$interval) {
+
+    var time;
+    var age;
+    var ts;
+
+      var called = 0;
+      $interval(function () {
+          called++;
+          console.log('called',called);
+      }, 1000);
+
+
+
+     var prueba = function(callback){
+
+        $interval(function(){
+            searchData('algo')
+              .then(
+                function(response){
+
+                  callback(response.data);
+                });
+
+        }, 1000);
+    }
+
+    var searchData = function(query, since) {
+        var data = {query: query};
+
+        var queryUrl = '/search';
+        var promise = $http.post(queryUrl, data)
+            .then(function (response) {
+            return response;
+        });
+        console.log('un mensaje');
+        return promise;
+    }
+
 
     return {
-        asyncSearch: function(query, since) {	
-            var data = {query: query};				
 
-            var queryUrl = '/search'; 
-            var promise = $http.post(queryUrl, data).then(function (response) {						
-                return response;
-            });
-            return promise;
-        }				
+        getCalled : function () {
+            return called;
+        },
+
+        init:function(options){
+            if(options.time && options.age){
+                time = options.time;
+                age = options.age;
+                return true
+            }else{
+                return false;
+            }
+
+        },
+
+        consult:function(callback){
+            $interval(function(){
+                searchData('algo')
+                .then(
+                    function(response){
+                    callback(response.data);
+                    });
+                }, 1000);
+        /*consult:searchData*/
+        },
+
+        asyncSearch: searchData
     };
 });
--- client/app/tweetservice/tweetservice.service.spec.js
+++ client/app/tweetservice/tweetservice.service.spec.js
@@ -6,31 +6,128 @@ describe('Service: tweetservice', function () {
   beforeEach(module('twittexpressApp'));
 
   // instantiate service
-  var tweetservice, $httpBackend;
-  beforeEach(inject(function (_tweetservice_, _$httpBackend_) {
+  var tweetservice,
+  $httpBackend,
+  options,
+  $interval;
+
+  beforeEach(inject(function (_tweetservice_, _$httpBackend_,$injector) {
+
     tweetservice = _tweetservice_;
     $httpBackend = _$httpBackend_;
+    $interval = $injector.get('$interval');
+
+
+    options = {
+        'time':1000,
+        'age':1
+    }
+
   }));
 
   afterEach(function () {
-      $httpBackend.verifyNoOutstandingRequest();
-      $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+    $httpBackend.verifyNoOutstandingExpectation();
   });
+
+
+  it('verify entry initial doptions ',function(){
+    var options = {
+        'time':1000,
+        'age':1
+    }
+    var result = tweetservice.init(options);
+    expect(result).toBe(true);
+  });
+
+
+
+
+
+  it('calls the interval callback', function () {
+
+
+    $httpBackend.expectPOST('/search')
+      .respond(200, []);
+
+      var ts;
+      tweetservice.consult(function(result){
+        console.log('valor de ts test:',result);
+        ts=result;
+
+
+        //$interval.flush(1000);
+        /*$httpBackend.flush(1000);
+        expect(ts.length).toBe(0);*/
+
+        /*
+
+        $httpBackend.flush();
+        expect(ts.length).toBe(0);
+
+        $interval.flush(1000);
+        $httpBackend.flush();
+        expect(ts.length).toBe(0);*/
+
+      });
+
+  });
+
+
+/*
+  it('should make a request based on the initial options',function(){
+
+
+    console.log('interval',$interval);
+
+  //  $httpBackend.expectPOST('/search')
+    //  .respond(200, []);
+
+    /*$interval(function(){
+      console.log('valoor');
+    },3000);*/
+
+
+
+//$httpBackend.flush();
+
+    //$interval.flush();
+/*
+    setInterval(function(){
+    $httpBackend.expectPOST('/search')
+      .respond(200, []);
+    var ts;
+      tweetservice.consult('algo')
+      .then(
+        function(response){
+          ts = response.data;
+        });
+      $httpBackend.flush();
+      expect(ts.length).toBe(0);
+    },2000);
+*/
+  //});
+
+
+
   /*
    * This service should poll the server each n time to check
    * if it has new twitts for today
    * */
+
   it('should poll a server', function () {
+
+    $httpBackend.expectPOST('/search')
+      .respond(200, []);
+
     var ts;
-    tweetservice.asyncSearch('algo').then(
+    tweetservice.asyncSearch('algo')
+    .then(
       function(response){
         ts = response.data;
-      }
-    );
-    $httpBackend.expectGET('/search?hashtag=algo')
-          .respond(200, []);
-    $httpBackend.flush();
+      });
 
+    $httpBackend.flush();
     expect(ts.length).toBe(0);
   });
 
